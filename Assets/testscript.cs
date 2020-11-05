5186ff191d6d92c37079d28797e8f998d65552fc
ustubyo <72735425+ustubyo@users.noreply.github.com>
2020-11-05 14:01:45 +0800

    Initial commit

---
+++ Assets/testscript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testscript : MonoBehaviour
+{
+    public GameObject scoreText;
+    float counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = ("Start Function");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            scoreText.GetComponent<Text>().text = ("Spacebar:" + counter);
+            print("Spacebar counter" + counter);
+            counter++;
+        }
+
+      /*  if (Input.GetKeyUp(KeyCode.Space))
+        {
+            print("Spacebar counter");
+        }*/
+    }
+    
+    
+}
